6ef69c647905cadcc7163fe1fcee5942ab9ccc7f
DSJ <2926505295@qq.com>
2026-07-11 00:39:23 +0800

    Initial commit

---
+++ src/main/java/injection/MixinGameRenderer.java
@@ -0,0 +1,48 @@
+package injection;
+
+import cn.remix.event.impl.Render3DEvent;
+import cn.remix.module.impl.render.NoHurtCam;
+import cn.remix.util.IMinecraft;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.render.*;
+import net.minecraft.client.util.math.MatrixStack;
+import org.joml.Matrix4f;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GameRenderer.class)
+public abstract class MixinGameRenderer implements IMinecraft {
+
+    @Shadow
+    public abstract Camera getCamera();
+
+    @Shadow
+    @Final
+    private BufferBuilderStorage buffers;
+
+    @Inject(method = "renderWorld", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/GameRenderer;renderHand(FZLorg/joml/Matrix4f;)V"))
+    private void renderWorld(RenderTickCounter renderTickCounter, CallbackInfo ci, @Local(ordinal = 0) Matrix4f projectionMatrix, @Local(ordinal = 1) Matrix4f modelViewMatrix) {
+        MatrixStack matrixStack = new MatrixStack();
+        VertexConsumerProvider consumers = this.buffers.getEntityVertexConsumers();
+        Render3DEvent event = new Render3DEvent(matrixStack, consumers, renderTickCounter.getTickProgress(true), projectionMatrix, modelViewMatrix);
+        Camera camera = this.getCamera();
+        RenderSystem.getModelViewStack().pushMatrix().mul(matrixStack.peek().getPositionMatrix());
+        Matrix4f entryMatrix = matrixStack.peek().getPositionMatrix();
+        entryMatrix.rotateX((float) Math.toRadians(camera.getPitch()));
+        entryMatrix.rotateY((float) Math.toRadians(camera.getYaw() + 180));
+        instance.getEventManager().call(event);
+        RenderSystem.getModelViewStack().popMatrix();
+    }
+
+    @Inject(at = @At("HEAD"), method = "tiltViewWhenHurt(Lnet/minecraft/client/util/math/MatrixStack;F)V", cancellable = true)
+    private void tiltViewWhenHurt(MatrixStack matrices, float tickProgress, CallbackInfo ci) {
+        if (instance.getModuleManager().getModule(NoHurtCam.class).isEnabled()) {
+            ci.cancel();
+        }
+    }
+}
---
+++ src/main/java/injection/MixinInGameHud.java
@@ -0,0 +1,49 @@
+package injection;
+
+import cn.remix.event.impl.Render2DEvent;
+import cn.remix.module.impl.render.HUD;
+import cn.remix.util.IMinecraft;
+import cn.remix.util.misc.TimerUtil;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.gui.render.state.GuiRenderState;
+import net.minecraft.client.render.RenderTickCounter;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(InGameHud.class)
+public abstract class MixinInGameHud implements IMinecraft {
+
+    @Unique
+    private final GuiRenderState cachedHudState = new GuiRenderState();
+
+    @Unique
+    private final TimerUtil timer = new TimerUtil();
+
+    @Inject(method = "render", at = @At(value = "HEAD"))
+    private void render(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
+        if (mc.player == null || mc.world == null) return;
+
+        if (timer.hasTimeElapsed(1000L / instance.getModuleManager().getModule(HUD.class).getHudFps().getValue())) {
+            timer.reset();
+            cachedHudState.clear();
+            DrawContext cacheContext = new DrawContext(mc, cachedHudState, mc.getWindow().getScaledWidth(), mc.getWindow().getScaledHeight());
+            instance.getEventManager().call(new Render2DEvent(cacheContext, tickCounter.getTickProgress(false)));
+        }
+
+        cachedHudState.forEachSimpleElement(context.state::addSimpleElement, GuiRenderState.LayerFilter.ALL);
+    }
+
+    @Inject(method = "renderStatusEffectOverlay", at = @At("HEAD"), cancellable = true)
+    private void renderStatusEffectOverlay(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
+        HUD hud = instance.getModuleManager().getModule(HUD.class);
+        if (hud.isEnabled() && hud.getNoPotionIcons().getValue()) {
+            ci.cancel();
+        }
+    }
+}
+
+
